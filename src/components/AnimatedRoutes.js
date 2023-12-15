8779ae016cf76354841c5be26a86e22749aa5aeb
Bhavdeep Arora <bhavdeepsa@gmail.com>
2023-12-15 12:57:45 -0500

    transition changes

--- src/components/AnimatedRoutes.js
+++ src/components/AnimatedRoutes.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Projects from '../pages/Projects';
 import Experience from '../pages/Experience';
 import ContactMe from '../pages/ContactMe';
 import MyResume from '../pages/MyResume';
-import { AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
 
 function AnimatedRoutes() {
     const location = useLocation();
 
     return (
-        <AnimatePresence>
-            <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/experience" element={<Experience />} />
-                <Route path="/contactme" element={<ContactMe />} />
-                <Route path="/resume" element={<MyResume />} />
-            </Routes>
+        <AnimatePresence mode='wait'>
+            <motion.div
+                key={location.pathname}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+            >
+                <Routes location={location} key={location.pathname}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/experience" element={<Experience />} />
+                    <Route path="/contactme" element={<ContactMe />} />
+                    <Route path="/resume" element={<MyResume />} />
+                </Routes>
+            </motion.div>
         </AnimatePresence>
-    )
+    );
 }
 
-export default AnimatedRoutes
+export default AnimatedRoutes;
